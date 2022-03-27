c2ede0248f34bd14718017cf60d7d864b49632e4
s.saparbekov <s.saparbekov@tinkoff.ru>
2022-03-27 16:10:21 +0300

    feat: completed task 3.1

--- src/main/java/ConsolePrinter.java
+++ src/main/java/ConsolePrinter.java
@@ -1,5 +1,21 @@
+/**
+ * {@code ConsolePrinter} выводит сообщения на консоль
+ *
+ * @author s.saparbekov
+ * **/
+
 public class ConsolePrinter {
+
+    /**
+     * Принимает строку и выводит на консоль приветственное сообщение
+     * "Hello, {@code message}!!!". Метод содержит как минимум два сайд эффекта
+     *          <p> 1. при вызове {@code System.out.println} изменяется состояние машины </p>
+     *          <p> 2. внутри метода изменяется значение переданного аргумента {@code message} </p>
+     *
+     * @param message строка, которая будет изменена и выведена на консоль.
+     * **/
     public static void print(String message) {
+        message = "Hello, " + message + "!!!";
         System.out.println(message);
     }
 }
