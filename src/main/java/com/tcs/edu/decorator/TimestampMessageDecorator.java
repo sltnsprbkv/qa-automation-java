2decad1a0a44baeeb3ae587f259f42346cb0aeff
s.saparbekov <s.saparbekov@tinkoff.ru>
2022-04-10 21:27:22 +0300

    feat: completed task 5.2

--- src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -11,15 +11,18 @@ import java.time.Instant;
 public class TimestampMessageDecorator {
 
     public static int messageCount = 0;
+    public static int PAGE_SIZE = 3;
     /**
      * Метод возвращает строку с счетчиком (messageCount) и текущим временем,
-     * и увеличивает счетчик
+     * и увеличивает счетчик. При выводе разделяет "страницы" согласно PAGE_SIZE.
      *
      * @param message строка, которая будет сконкатинирована с текущим временем
      * **/
     public static String decorate(String message) {
         messageCount++;
         final var decoratedMessage = String.format("%d %s %s", messageCount, Instant.now(), message);
-        return decoratedMessage;
+        return messageCount % PAGE_SIZE == 0
+                ? decoratedMessage + "\n---"
+                : decoratedMessage;
     }
 }
