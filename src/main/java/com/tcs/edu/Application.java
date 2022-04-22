c02733bc63fa976f0abd4a8d1b617eae752ff98f
s.saparbekov <s.saparbekov@tinkoff.ru>
2022-04-22 18:36:25 +0300

    feat: complete task 7.2

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -1,5 +1,6 @@
 package com.tcs.edu;
 
+import com.tcs.edu.model.MessageOrder;
 import com.tcs.edu.model.Severity;
 import com.tcs.edu.service.MessageService;
 
@@ -9,5 +10,9 @@ class Application {
         MessageService.print(Severity.MINOR, "dsadsa", null);
         MessageService.print(Severity.REGULAR, "zxcxz", "hfgfh", "plmd");
         MessageService.print(Severity.MAJOR, "null");
+
+        System.out.println("\n\n\n");
+        MessageService.print(Severity.REGULAR, MessageOrder.DESC, "message 1",
+                "message 2", "message 3", "message 4", "message 5", null, "message 7");
     }
 }
--- src/main/java/com/tcs/edu/service/MessageService.java
+++ src/main/java/com/tcs/edu/service/MessageService.java
@@ -4,10 +4,12 @@ import com.tcs.edu.decorator.NumerateMessageDecorator;
 import com.tcs.edu.decorator.SeparateDecorator;
 import com.tcs.edu.decorator.SeverityMessageDecorator;
 import com.tcs.edu.decorator.TimestampMessageDecorator;
+import com.tcs.edu.model.MessageOrder;
 import com.tcs.edu.model.Severity;
 import com.tcs.edu.printer.ConsolePrinter;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Objects;
 
 /**
@@ -15,17 +17,38 @@ import java.util.Objects;
  *
  * @author s.saparbekov
  * **/
+
 public class MessageService {
 
     /**
      * Метод выводит декорированные сообщения, кроме значений null
      *
-     * @param messages строка, которая будет сконкатинирована с ее уровнем важности
      * @param severity уровень важности
+     * @param messages декорируемая строка
      *
      * **/
     public static void print(Severity severity, String... messages) {
         Arrays.stream(messages)
+                .filter(Objects::nonNull)
+                .map(TimestampMessageDecorator::decorate)
+                .map(NumerateMessageDecorator::decorate)
+                .map(message -> SeverityMessageDecorator.decorate(severity, message))
+                .map(SeparateDecorator::decorate)
+                .forEach(ConsolePrinter::print);
+    }
+
+    /**
+     * Метод выводит отсортированные декорированные сообщения, кроме значений null
+     *
+     * @param severity уровень важности
+     * @param messageOrder порядок сортировки
+     * @param messages декорируемая строка
+     *
+     * **/
+    public static void print(Severity severity, MessageOrder messageOrder, String... messages) {
+        var finalMessages = Arrays.asList(messages);
+        if (messageOrder.equals(MessageOrder.DESC)) Collections.reverse(finalMessages);
+        finalMessages.stream()
                 .filter(Objects::nonNull)
                 .map(TimestampMessageDecorator::decorate)
                 .map(NumerateMessageDecorator::decorate)
