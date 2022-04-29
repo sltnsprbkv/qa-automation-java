2b12a5e95bb892acdccb7226b9040796563211af
s.saparbekov <s.saparbekov@tinkoff.ru>
2022-04-29 20:44:38 +0300

    feat: complete task 8.1

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -1,5 +1,6 @@
 package com.tcs.edu;
 
+import com.tcs.edu.domain.Message;
 import com.tcs.edu.model.Doubling;
 import com.tcs.edu.model.MessageOrder;
 import com.tcs.edu.model.Severity;
@@ -7,17 +8,29 @@ import com.tcs.edu.service.MessageService;
 
 class Application {
     public static void main(String[] args) {
-        MessageService.print(Severity.MAJOR, "qwewqewq");
-        MessageService.print(Severity.MINOR, "dsadsa", null);
-        MessageService.print(Severity.REGULAR, "zxcxz", "hfgfh", "plmd");
-        MessageService.print(Severity.MAJOR, "null");
+        MessageService.print(new Message("message 1", Severity.MINOR));
+        MessageService.print(new Message("message 2", Severity.MAJOR), null); // Здесь пропускаем null
+        MessageService.print(
+                new Message("message 2", Severity.MAJOR),
+                new Message("message 3"),
+                new Message("message 4", Severity.REGULAR));
+//        MessageService.print(new Message("😎", null)); // Тут выбрасывается исключение
 
         System.out.println("\n\n\n");
-        MessageService.print(Severity.REGULAR, MessageOrder.DESC, "message 1",
-                "message 2", "message 3", "message 4", "message 5", null, "message 7");
+        MessageService.print(MessageOrder.DESC,
+                new Message("😪", Severity.MINOR),
+                new Message("😑", Severity.REGULAR),
+                new Message("😃", Severity.MAJOR),
+                new Message("😎" )
+        );
 
         System.out.println("\n\n\n");
-        MessageService.print(Severity.REGULAR, MessageOrder.DESC, Doubling.DISTINCT, "message 1",
-                "message 2", "message 5", "message 5", "message 5", null, "message 7");
+        MessageService.print(MessageOrder.DESC, Doubling.DISTINCT,
+                new Message("😪", Severity.MINOR),
+                new Message("😎" ),
+                new Message("😑", Severity.REGULAR),
+                new Message("😃", Severity.MAJOR),
+                new Message("😎" )
+        );
     }
 }
---
+++ src/main/java/com/tcs/edu/domain/Message.java
@@ -0,0 +1,92 @@
+package com.tcs.edu.domain;
+
+import com.tcs.edu.model.Severity;
+
+import java.util.Objects;
+
+/**
+ * Представление сообщений в виде объектов
+ *
+ * @author s.saparbekov
+ */
+
+public class Message {
+
+    private final String body;
+    private final Severity severity;
+
+    /**
+     * Конструктор класса Message
+     *
+     * @param severity уровень важности, не может быть null
+     * @param body тело сообщения, не может быть null
+     *
+     * **/
+    public Message(String body, Severity severity) throws NullPointerException {
+        if (body == null || severity == null) {
+            throw new NullPointerException("body or severity of message is null");
+        }
+        this.body = body;
+        this.severity = severity;
+    }
+
+    /**
+     * Конструктор класса Message. По-умолчанию уровень важности - REGULAR.
+     *
+     * @param body тело сообщения, не может быть null
+     *
+     * **/
+    public Message(String body) throws NullPointerException {
+        if (body == null) {
+            throw new NullPointerException("body or severity of message is null");
+        }
+        this.body = body;
+        this.severity = Severity.REGULAR;
+    }
+
+    /**
+     * Получение тело сообщения.
+     *
+     * **/
+    public String getBody() {
+        return body;
+    }
+
+    /**
+     * Получение уровня важности сообщения.
+     *
+     * **/
+    public Severity getSeverity() {
+        return severity;
+    }
+
+    /**
+     * Операция сравнения сообщений.
+     *
+     * @param object сравниваемый объект
+     *
+     * **/
+    @Override
+    public boolean equals(Object object) {
+        if (object == this) {
+            return true;
+        }
+
+        if (!(object instanceof Message)) {
+            return false;
+        }
+
+        Message message = (Message) object;
+
+        return (this.body.equals(message.body) && this.severity.equals(message.severity));
+    }
+
+    /**
+     * Операция создания хеша объекта.
+     *
+     * **/
+    @Override
+    public int hashCode() {
+        return Objects.hash(body, severity);
+    }
+}
