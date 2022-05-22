6484f1714de882fcce569369c370281154326b73
s.saparbekov <s.saparbekov@tinkoff.ru>
2022-05-22 21:58:42 +0300

    feat: complete task 10

--- src/main/java/com/tcs/edu/service/ValidatedService.java
+++ src/main/java/com/tcs/edu/service/ValidatedService.java
@@ -2,7 +2,8 @@ package com.tcs.edu.service;
 
 public abstract class ValidatedService {
 
-    public boolean isArgsValid(String message) {
-        return message != null;
+    public void isArgsValid(String message) {
+        if (message == null || message.equals("null")) throw new IllegalArgumentException("message is null");
+        if (message.length() == 0) throw new IllegalArgumentException("message is empty");
     }
 }
