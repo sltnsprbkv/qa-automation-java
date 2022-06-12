03173a010cbe86bbdae45492b402ec66cea5ccc9
s.saparbekov <s.saparbekov@tinkoff.ru>
2022-06-12 20:56:49 +0300

    feat: completed task 13

---
+++ src/test/java/com/tcs/edu/rest/CountryResourceTest.java
@@ -0,0 +1,77 @@
+package com.tcs.edu.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class CountryResourceTest extends RestApiBaseTest {
+
+    @Test
+    void getApiCountriesIdTest() {
+
+        given().
+                spec(requestSpec).
+        when().
+                get("/api/countries/1").
+        then().
+                assertThat().
+                statusCode(200);
+    }
+
+    @Test
+    void postApiCountriesTest() {
+
+        given().
+                spec(requestSpec).
+                contentType("application/json").
+                body("""
+                        {
+                            "countryName": "st"
+                        }
+                        """).
+        when().
+                post("/api/countries").
+        then().
+                log().all().
+                assertThat().
+                statusCode(201);
+    }
+
+    @Test
+    void putApiCountriesIdTest() {
+
+        given().
+                spec(requestSpec).
+                contentType("application/json").
+                body("""
+                        {
+                          "id": 2,
+                          "countryName": "sa"
+                        }
+                        """).
+        when().
+                put("/api/countries/2").
+        then().
+                log().all().
+                assertThat().
+                statusCode(200);
+    }
+
+    @Test
+    void deleteApiCountriesIdTest() {
+
+        given().
+                spec(requestSpec).
+                body("""
+                        {
+                          "id": 3,
+                          "countryName": "sa"
+                        }
+                        """).
+        when().
+                delete("/api/countries/3").
+        then().
+                assertThat().
+                statusCode(204);
+    }
+}
